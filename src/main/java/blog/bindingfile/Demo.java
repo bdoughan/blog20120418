4f32b886a0b626ae0c7f1173c6d861ea1b61b5d1
Blaise Doughan <blaise.doughan@gmail.com>
2012-06-29 10:42:15 -0400

    Updated example to match the released EclipseLink 2.4.0.

--- src/main/java/blog/bindingfile/Demo.java
+++ src/main/java/blog/bindingfile/Demo.java
@@ -1,22 +1,23 @@
 package blog.bindingfile;
 
-import java.io.File;
+import java.io.FileInputStream;
 import java.util.*;
 import javax.xml.bind.*;
 import javax.xml.transform.stream.StreamSource;
-import org.eclipse.persistence.jaxb.JAXBContextFactory;
+import org.eclipse.persistence.jaxb.JAXBContextProperties;
 
 public class Demo {
-
+    
     public static void main(String[] args) throws Exception {
         Map<String, Object> properties = new HashMap<String, Object>(3);
-        properties.put(JAXBContextFactory.ECLIPSELINK_OXM_XML_KEY, "blog/bindingfile/binding.json");
-        properties.put("eclipselink.media-type", "application/json");
-        properties.put("eclipselink.json.include-root", false);
+        properties.put(JAXBContextProperties.OXM_METADATA_SOURCE, "blog/bindingfile/binding.json");
+        properties.put(JAXBContextProperties.MEDIA_TYPE, "application/json");
+        properties.put(JAXBContextProperties.JSON_INCLUDE_ROOT, false);
         JAXBContext jc = JAXBContext.newInstance("blog.bindingfile", Customer.class.getClassLoader() , properties);
 
+
         Unmarshaller unmarshaller = jc.createUnmarshaller();
-        StreamSource json = new StreamSource(new File("src/main/resources/blog/bindingfile/input.json"));
+        StreamSource json = new StreamSource(new FileInputStream("src/main/resources/blog/bindingfile/input.json"));
         Customer customer = (Customer) unmarshaller.unmarshal(json, Customer.class).getValue();
 
         Marshaller marshaller = jc.createMarshaller();
